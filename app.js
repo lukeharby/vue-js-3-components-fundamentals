3f6f8693a0717dc2881b145b419a2c17874e399e
Luke Harby <lukeharby@Lukes-MacBook-Pro.local>
2024-11-20 09:21:29 +0000

    change local from global

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-const app = Vue.createApp({
-  data() {
-    return {
-      plans: ['The Single', 'The Curious', 'Doppio']
-    }
-  }
-})
-.component('plan', {
+let PlanItemComponent = {
   template: '#plan-template',
   props: {
-    name: { type: String, default: 'The Single' }
+    name: { type: String, default: 'The Single' },
+    selected: { type: Boolean, default: false }
+  },
+  methods: {
+    select() {
+      this.$emit('select', this.name)
+    }
   }
+}
+
+let PlanPicker = {
+  components: { PlanItem: PlanItemComponent },
+  template: '#plan-picker-template',
+  data() {
+    return {
+      plans: ['The Single', 'The Curious', 'Doppio'],
+      selectedPlan: null
+    }
+  },
+  methods: {
+    selectPlan(plan) {
+      this.selectedPlan = plan
+    }
+  }
+}
+
+const app = Vue.createApp({
+  components: { PlanPicker: PlanPicker }
 })
 .mount('#app')
